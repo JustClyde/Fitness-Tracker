20fd6d82c6b1538dfcb8310701fd0962f1c85568
JustClyde <hlonelamadubela@gmail.com>
2025-01-09 16:16:55 +0200

    Update settings.py

--- fitnesstracker/settings.py
+++ fitnesstracker/settings.py
@@ -38,8 +38,6 @@ ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'tracker',
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
